de084601699bb8b7ee0b4b83c28640c88bc061f5
JoeCT_Chang <JoeCT.Chang@moxa.com>
2023-03-07 10:45:30 +0800

    initial commit"

---
+++ apps/nx-monorepo/src/app/hello/hello.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-sample-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css'],
+})
+export class HelloComponent {}
